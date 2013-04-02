29b15bc2be921fcaaf833788e64a5176f30ed6a6
Joshua Rose <joshrose@uoregon.edu>
2013-04-02 09:51:14 -0700

    Made menu floaty.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-
+$(window).scroll(function(e){
+	$menu = $('div.menu');
+	// if($(this).scrollTop() > 350 && $menu.css('position') != 'fixed') {
+	if($(this).scrollTop() > 350) {
+		$menu.css({
+			'position':'fixed',
+			'top':'0px'
+		});
+	}
+	else {
+		$menu.css({
+			'position':'relative'
+		});
+	}
+});
